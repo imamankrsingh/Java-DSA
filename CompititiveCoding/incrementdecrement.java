ec81c1d67d5929453921b74aaeab3785efdf0ecc
imamankrsingh <amankumarsingh5114@gmail.com>
2026-01-07 13:51:36 +0530

    added increment decrement

---
+++ CompititiveCoding/incrementdecrement.java
@@ -0,0 +1,14 @@
+
+
+public class incrementdecrement {
+    public static void main(String[] args) {
+        int x = 5;
+        
+        // System.out.println(x);
+        // System.out.println(++x); // iska matlab pahle update hogi phir print hogi value
+        
+        System.out.println(x++); // iska matlab pahle print hogi phir update hogi
+        System.out.println(x);
+
+    }
+}
