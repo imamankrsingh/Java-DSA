deaa0ea7c73a25c1427aa50ac3c135857f829c7c
imamankrsingh <amankumarsingh5114@gmail.com>
2026-01-13 11:00:46 +0530

    Day 11 - simpleinterest  programms are added

---
+++ operators/assignmentoperators.java
@@ -0,0 +1,27 @@
+public class assignmentoperators {
+    public static void main(String[] args) {
+        int p = 10;
+        int q;
+
+        //  = 
+        q=p;
+        System.out.println(q);
+
+        //  +=
+        p += q ;
+        System.out.println(p);
+
+        // -=
+        p -= q;
+        System.out.println(p);
+
+        //  *=
+        p *= q;
+        System.out.println(p);
+
+
+        //  /=
+        p /= q;
+        System.out.println(p);
+    }
+}
