b154ee86e16551377cf0179abd986c8d28edaeab
imamankrsingh <amankumarsingh5114@gmail.com>
2026-02-01 16:34:28 +0530

    Day 28 Missing numbers programms are added

--- CompititiveCoding/MissingNumber.java
+++ CompititiveCoding/MissingNumber.java
@@ -1,7 +1,7 @@
 public class MissingNumber {
     public static void main(String[] args) {
 
-        int arr[] = {1, 2, 4, 5};   
+        int arr[] = {1, 2, 5};   
         int n = arr.length + 1;    
 
         int expectedSum = n * (n + 1) / 2;
