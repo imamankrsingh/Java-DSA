307234cd01f19b3b9a39b74efa7745fc931a2f9b
imamankrsingh <amankumarsingh5114@gmail.com>
2026-02-02 12:25:58 +0530

    Day 29 Char frequency programms are added

---
+++ CompititiveCoding/CharFrequency.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CharFrequency {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        int freq[] = new int[256];   
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            freq[ch]++;   
+        }
+
+        System.out.println("Character Frequencies:");
+        for (int i = 0; i < 256; i++) {
+            if (freq[i] > 0) {
+                System.out.println((char) i + " = " + freq[i]);
+            }
+        }
+    }
+}
+
