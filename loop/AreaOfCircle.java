308a1877074963f49818678fc83d6bb00e856224
imamankrsingh <amankumarsingh5114@gmail.com>
2026-02-16 16:35:39 +0530

    Day 42 Refactor

--- loop/AreaOfCircle.java
+++ loop/AreaOfCircle.java
@@ -1,15 +1,27 @@
 import java.util.Scanner;
 
 public class AreaOfCircle {
+
+
+    public static double calculateArea(double radius) {
+        return Math.PI * radius * radius;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter radius: ");
         double r = sc.nextDouble();
 
-        double area = Math.PI * r * r ;
+        if (r < 0) {
+            System.out.println("Radius cannot be negative!");
+        } else {
+            double area = calculateArea(r);
+            System.out.printf("Area of Circle = %.2f", area);
+        }
 
-        System.out.println("Area of Circle = " + area);
+        sc.close();
     }
 }
+
 
