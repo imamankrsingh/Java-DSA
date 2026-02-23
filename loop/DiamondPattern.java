ebdc93d385cb921ca4588163f9bf8ccc5949a073
imamankrsingh <amankumarsingh5114@gmail.com>
2026-02-23 22:30:05 +0530

    Day 50 Diamond Pattern programms are added

--- loop/DiamondPattern.java
+++ loop/DiamondPattern.java
@@ -1,4 +1,4 @@
-public class ButterflyPattern {
+public class DiamondPattern {
     public static void main(String[] args) {
 
         int n = 4; 
@@ -6,36 +6,27 @@ public class ButterflyPattern {
         
         for(int i = 1; i <= n; i++) {
 
-            
-            for(int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-
-
-            for(int j = 1; j <= 2*(n - i); j++) {
+    
+            for(int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
-            
-            for(int j = 1; j <= i; j++) {
+        
+            for(int j = 1; j <= 2*i - 1; j++) {
                 System.out.print("*");
             }
 
             System.out.println();
         }
 
-
+    
         for(int i = n - 1; i >= 1; i--) {
 
-            for(int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-
-            for(int j = 1; j <= 2*(n - i); j++) {
+            for(int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
-            for(int j = 1; j <= i; j++) {
+            for(int j = 1; j <= 2*i - 1; j++) {
                 System.out.print("*");
             }
 
