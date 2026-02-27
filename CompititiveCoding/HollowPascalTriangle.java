b47b369a45f7ba59badcff7f27f474793afaa1a3
imamankrsingh <amankumarsingh5114@gmail.com>
2026-02-27 11:41:32 +0530

    Day 54 Hollow Pascal Triangle programms are added

---
+++ CompititiveCoding/HollowPascalTriangle.java
@@ -0,0 +1,33 @@
+public class HollowPascalTriangle {
+    public static void main(String[] args) {
+
+        int n = 4;
+
+        for(int i = 1; i <= n; i++) {
+
+            for(int j = 1; j <= i; j++) {
+
+                if(i == 1 || i == n || j == 1 || j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+
+        for(int i = n - 1; i >= 1; i--) {
+
+            for(int j = 1; j <= i; j++) {
+
+                if(i == 1 || j == 1 || j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
