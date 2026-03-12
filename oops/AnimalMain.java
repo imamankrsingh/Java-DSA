81968628fea265b007dcc55d466660fe251353fb
imamankrsingh <amankumarsingh5114@gmail.com>
2026-03-12 12:42:54 +0530

    Day 66 Animal programms are added

---
+++ oops/AnimalMain.java
@@ -0,0 +1,37 @@
+class Animal {
+
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+
+}
+
+class Dog extends Animal {
+
+    void sound() {
+        System.out.println("Dog barks");
+    }
+
+}
+
+class Cat extends Animal {
+
+    void sound() {
+        System.out.println("Cat meows");
+    }
+
+}
+
+public class AnimalMain {
+
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+        Cat c = new Cat();
+
+        d.sound();
+        c.sound();
+
+    }
+
+}
