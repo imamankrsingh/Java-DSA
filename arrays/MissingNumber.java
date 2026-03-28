2f1c73ddda46a8b24357dad7ffb3f02832aff43e
imamankrsingh <amankumarsingh5114@gmail.com>
2026-03-28 10:19:20 +0530

    Day 80 Missing Number programms are added

---
+++ arrays/MissingNumber.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class MissingNumber {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n - 1];
+
+        System.out.println("Enter elements:");
+
+        for(int i = 0; i < n - 1; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Total sum
+        int total = n * (n + 1) / 2;
+
+        // Array sum
+        int sum = 0;
+        for(int i = 0; i < n - 1; i++) {
+            sum += arr[i];
+        }
+
+        int missing = total - sum;
+
+        System.out.println("Missing Number: " + missing);
+    }
+}
