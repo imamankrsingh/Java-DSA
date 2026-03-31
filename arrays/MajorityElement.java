82fd16f17f26b98d17efa074d061ba94041fdadc
imamankrsingh <amankumarsingh5114@gmail.com>
2026-03-31 13:59:06 +0530

    Day 83 Majority Elements programms are added

---
+++ arrays/MajorityElement.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class MajorityElement {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter elements:");
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        
+        int candidate = arr[0];
+        int count = 1;
+
+        for(int i = 1; i < n; i++) {
+
+            if(arr[i] == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+
+            if(count == 0) {
+                candidate = arr[i];
+                count = 1;
+            }
+        }
+
+    
+        count = 0;
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == candidate) {
+                count++;
+            }
+        }
+
+        if(count > n / 2) {
+            System.out.println("Majority Element: " + candidate);
+        } else {
+            System.out.println("No Majority Element");
+        }
+    }
+}
