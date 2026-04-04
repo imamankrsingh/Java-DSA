d8e62d7161e7b79ad7d6d93437864b0ed907c230
imamankrsingh <amankumarsingh5114@gmail.com>
2026-04-04 13:19:08 +0530

    Day 85 Methods Algebra programms are added

---
+++ methods/main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Algebra {
+    int add(int a, int b) {
+        int ans = a + b;
+        return ans;
+    }
+}
+
+public class main {
+    public static void main(String[] args) {
+        Algebra obj = new Algebra();
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        System.out.println("Sum of input numbers is ");
+        int ans = obj.add(x,y);
+        System.out.println(ans);
+    
+    }
+}
