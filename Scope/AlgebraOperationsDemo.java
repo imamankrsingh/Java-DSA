8d9f92d5860caa9b56b4b45039faea75e5a88508
imamankrsingh <amankumarsingh5114@gmail.com>
2026-04-06 12:56:41 +0530

    day 87 Algebra Operations demo programms are added

---
+++ Scope/AlgebraOperationsDemo.java
@@ -0,0 +1,42 @@
+class Algebra {
+
+    int a = 10;
+    int b = 20;
+
+    int add() {
+        int p = 100;
+        int q = 200;
+        return p + q;
+    }
+
+    int sub() {
+        return a - b;   // अब a और b defined हैं
+    }
+
+    void demo() {
+        int a = 5;
+        System.out.println(a);
+
+        for (int i = 0; i < 5; i++) {
+            int b = 10;
+            System.out.println(b);
+        }
+
+        {
+            int b = 100;
+            System.out.println(b);
+        }
+    }
+}
+
+public class AlgebraOperationsDemo {   // class name capital होना चाहिए (best practice)
+    public static void main(String[] args) {
+
+        Algebra obj = new Algebra();
+
+        obj.demo();
+
+        System.out.println("Addition = " + obj.add());
+        System.out.println("Subtraction = " + obj.sub());
+    }
+}
---
+++ arrays/main.java
@@ -0,0 +1,18 @@
+
+import javax.sound.sampled.SourceDataLine;
+
+class ArrayExamples{
+    void demoArrays(){
+        int[] ages = new int {3};
+        float{} weights = new float[3];
+        String[] names = new String[3];
+
+        ages[0] = 34;
+        ages[1] = 12;
+        ages[2] = 45;
+
+        System.out.println("Ages[0]");
+        System.out.println("Ages[1]");
+        System.out.println("Ages[2]");
+    }
+}
