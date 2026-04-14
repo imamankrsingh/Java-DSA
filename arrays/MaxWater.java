46bf3b759cbc19b19e460367c01ffccc483b838a
imamankrsingh <amankumarsingh5114@gmail.com>
2026-04-14 19:46:25 +0530

    Day 93 Max Water programms are added

---
+++ arrays/MaxWater.java
@@ -0,0 +1,24 @@
+public class MaxWater {
+    public static void main(String[] args) {
+        int[] height = {1,8,6,2,5,4,8,3,7};
+
+        int left = 0, right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int h = Math.min(height[left], height[right]);
+            int width = right - left;
+            int area = h * width;
+
+            maxArea = Math.max(maxArea, area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        System.out.println("Max Area = " + maxArea);
+    }
+}
