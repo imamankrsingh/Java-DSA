f35acca9b51c28ac13941ed0af0d20a156831077
imamankrsingh <amankumarsingh5114@gmail.com>
2026-05-05 12:31:13 +0530

    day 114 SOD Recursion programms are added

---
+++ Recursion/SOD.java
@@ -0,0 +1,17 @@
+public class SOD {
+    static int sod(int n) {
+        if (n >= 0 && n <= 9) {
+            return n;
+        }
+
+        return sod(n / 10) + n % 10;
+
+        // int smallAns = sod(n /10);
+
+        // return smallAns + n  % 10;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sod(12345));
+    }
+}
