33cc0e9f99b3b54ed822253815023578d6842272
imamankrsingh <amankumarsingh5114@gmail.com>
2026-05-07 20:43:45 +0530

    day 116 Multiples Recursion programms are added

--- Recursion/Multiples.java
+++ Recursion/Multiples.java
@@ -1,26 +1,3 @@
-// import java.util.Scanner;
-
-// public class Multiples {
-
-//     static void printMultiples(int n, int k ) {
-//         if (n == k) {
-//             System.out.println(n);
-//             return;
-//         }
-        
-//         printMultiples(n, k-1);
-//             System.out.println(n * k);
-//     }
-
-//     public static void main (String[] args) {
-//         Scanner sc = new Scanner(System.in);
-//         int n = sc.nextInt();
-//         int k = sc.nextInt();
-//         printMultiples(n, k);
-//     }
-    
-//     }
-
 import java.util.Scanner;
 
 public class Multiples {
