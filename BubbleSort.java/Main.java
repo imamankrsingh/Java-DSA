2b78ce4ec052dde3170dc082c416b5c3545ceb6d
imamankrsingh <amankumarsingh5114@gmail.com>
2026-05-12 15:19:31 +0530

    Day 121 Bubble Sort Programms are added

---
+++ BubbleSort.java/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    
+    static void bubbleSort(int[] a) {
+        int n = a.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+
+                if (a[j] > a[j + 1]) {
+                    
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] a = {7 , 6 ,5, 4 ,3};
+        bubbleSort(a);
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
+    }
+
+}
