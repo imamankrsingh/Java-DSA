ea66c3ee15c064725914a0f9efb17afd2575c712
imamankrsingh <amankumarsingh5114@gmail.com>
2026-06-13 20:33:17 +0530

    Day 150 Sort Array programms are added

---
+++ Sorting/SortArr.java
@@ -0,0 +1,37 @@
+public class SortArr {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 9, 1, 5, 6};
+        sortArray(arr);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    static void SortArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void sortArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
